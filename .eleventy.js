698e36a5b359e137db5804f6a592654822020b44
Max Luebbers <luebbersma@gmail.com>
2020-01-03 14:21:52 -0500

    (FEAT) Added tag support.

--- .eleventy.js
+++ .eleventy.js
@@ -51,6 +51,7 @@ module.exports = function(eleventyConfig) {
         dir: {
             input: "src",
             output: "public",
+            data: "data",
             layouts: "./layouts",
             includes: "./includes"
         }
